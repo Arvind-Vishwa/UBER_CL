0a426eeefd87f6694cc37a6b27928317f73896b5
Arvind-Vishwa <av0216380@gmail.com>
2025-11-03 07:05:52 +0530

    Added mongoDB

--- Backend/app.js
+++ Backend/app.js
@@ -3,9 +3,10 @@ dotenv.config();
 const express=require('express');
 const cors=require('cors');
 const app=express();
-
+const connectTodb=require('./db/db.js');
 app.use(cors());
 
+connectTodb();
 app.get('/home',(req,res)=>{
     res.send("hii");
 });
