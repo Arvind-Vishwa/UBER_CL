b42466f80c3773dbf8da20bc27533121133d2140
Arvind-Vishwa <av0216380@gmail.com>
2025-11-26 23:18:35 +0530

    added profile

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ const dotenv=require('dotenv');
 dotenv.config();
 const express=require('express');
 const cors=require('cors');
+const cookieParser=require('cookie-parser');
 const app=express();
 const connectTodb=require('./db/db.js');
 const userRoutes=require('./routes/user.routes.js');
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.use(cookieParser);
 
 app.get('/home',(req,res)=>{
     res.send("hii");
