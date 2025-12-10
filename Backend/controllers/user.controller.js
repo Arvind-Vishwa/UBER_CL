65c1742fb6d8be55aaf0128f0b73704f31436a22
Arvind-Vishwa <av0216380@gmail.com>
2025-12-10 10:26:34 +0530

    added captain

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,66 +1,79 @@
 const userModel = require('../models/user.model');
-const userService = require('../services/user.services');
+const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
-const blackListTokenModel=require('../models/blacklistToken.model');
+const blackListTokenModel = require('../models/blackListToken.model');
 
 module.exports.registerUser = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
 
-  const { fullname, email, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-  // Create user instance just to use hashing method
-  const user = new userModel({
-    fullname,
-    email,
-    password
-  });
+    const { fullname, email, password } = req.body;
 
-  // Hash password using schema method
-  user.password = await user.hashPassword(password);
-  
-  await user.save();
+    const isUserAlready = await userModel.findOne({ email });
 
-  const token = user.generateAuthToken();
-  res.status(201).json({ token, user });
-};
+    if (isUserAlready) {
+        return res.status(400).json({ message: 'User already exist' });
+    }
+
+    const hashedPassword = await userModel.hashPassword(password);
+
+    const user = await userService.createUser({
+        firstname: fullname.firstname,
+        lastname: fullname.lastname,
+        email,
+        password: hashedPassword
+    });
+
+    const token = user.generateAuthToken();
+
+    res.status(201).json({ token, user });
 
 
-module.exports.loginUser = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const { email, password } = req.body;
-//   console.log(req.body);
-  const user = await userModel.findOne({ email }).select('+password');
-  if (!user) {
-    return res.status(401).json({ message: 'Invalid email or password' });
-  }
-
-  const isMatch = await user.comparePassword(password);
-  if (!isMatch) {
-    return res.status(401).json({ message: 'Invalid email or password' });
-  }
-
-  const token = user.generateAuthToken();
-  res.cookie('token',token);
-  res.status(200).json({ token, user });
-};
-
-module.exports.getUserProfile=async(req,res,next) =>{
-
-  res.status(200).json(req.user);
 }
 
-module.exports.logoutUser=async (req,res,next) =>{
+module.exports.loginUser = async (req, res, next) => {
 
-  res.clearCookie('token');
-  const token=req.cookie.token || req.headers.authorization.split(' ')[1];
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-  await blackListTokenModel.create({token});
-  res.status(200).json({message:'Logged out'});
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select('+password');
+
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = user.generateAuthToken();
+
+    res.cookie('token', token);
+
+    res.status(200).json({ token, user });
+}
+
+module.exports.getUserProfile = async (req, res, next) => {
+
+    res.status(200).json(req.user);
+
+}
+
+module.exports.logoutUser = async (req, res, next) => {
+    res.clearCookie('token');
+    const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
+
+    await blackListTokenModel.create({ token });
+
+    res.status(200).json({ message: 'Logged out' });
+
 }
